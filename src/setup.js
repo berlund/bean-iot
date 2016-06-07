61fb36521a16eb05bc85d27be737888b82d8f914
Lars Engel <lars.engel@gmail.com>
2016-06-07 22:57:16 +0200

    Write generated certs to file

--- src/setup.js
+++ src/setup.js
@@ -1,3 +1,8 @@
+/**
+ * This script is meant to setup a new bean iot device including
+ * policies and certs. Right now we're just creating the certs.
+ */
+var fs = require('fs');
 var AWS = require('aws-sdk');
 var iot = new AWS.Iot({ region: 'eu-central-1' });
 
@@ -6,39 +11,54 @@ var certArn;
 var params = {
     setAsActive: true
 };
+
+function writeFile(path, content) {
+    return new Promise(function (resolve, reject) {
+        fs.writeFile(path, content, err => {
+            if (err) {
+                console.log('Could not write file: ', path)
+                reject(err);
+            } else {
+                resolve(path);
+            }
+        })
+    });
+}
+
+function writeCerts(certs) {
+    var p1 = writeFile('./certs/certificate.pem.crt', certs.certificatePem);
+    var p2 = writeFile('./certs/private.pem.crt', certs.keyPair.PrivateKey);
+    var p3 = writeFile('./certs/public.pem.crt', certs.keyPair.PublicKey);
+    return Promise.all([p1, p2, p3]);
+}
+
+function attachPrincipalPolicy(certificateArn) {
+    //TODO fetch policy from AWS or create a new one
+    var principalParams = {
+        principal: certificateArn,
+        policyName: 'bluebean-Policy'
+    }
+    return iot.attachPrincipalPolicy(principalParams)
+        .promise()
+}
+
+function attachThingPrincipal(certificateArn) {
+    var principalParams = {
+        principal: certificateArn,
+        thingName: 'bluebean'
+    }
+    return iot.attachThingPrincipal(principalParams)
+        .promise()
+}
+
 iot.createKeysAndCertificate(params)
     .promise()
     .then(result => {
-        //console.log(result);
+        console.log(result);
         //TODO: save keys and cert to file
-        return Promise.resolve(result.certificateArn);
-    })
-    .then(arn => {
-        var principalParams = {
-            principal: arn,
-            policyName: 'bluebean-Policy'
-        }
-        iot.attachPrincipalPolicy(principalParams)
-            .promise()
-            .then(r => {
-
-                console.log(r);
-                return Promise.resolve(r)
-            })
-            .catch(e => { console.log(e) })
-        return Promise.resolve(arn)
-    })
-    .then(r => {
-        var principalParams = {
-            principal: r,
-            thingName: 'bluebean'
-        }
-        iot.attachThingPrincipal(principalParams)
-            .promise()
-            .then(r2 => {
-                console.log(r2);
-                return Promise.resolve(r2)
-            })
-            .catch(e => { console.log(e) })
+        return Promise.all([
+            writeCerts(result),
+            attachPrincipalPolicy(result.certificateArn),
+            attachThingPrincipal(result.certificateArn)]);
     })
     .catch(e => { console.log(e) });
